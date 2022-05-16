f3e6ec3ca412da4fbf2bfccebdd3c64194b0f140
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-05-16 13:03:11 +0600

    db connected

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
@@ -7,6 +8,21 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://dbPlantPlanet:R6fioYRhJlhwjSWE@cluster0.rjowz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+try{
+    await client.connect();
+    const productCollection = client.db('plantPlanet').collection('product');
+}
+finally{}
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('plant-planet server running')
 });
