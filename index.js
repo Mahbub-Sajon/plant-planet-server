bdbef7d34ee80bc3c807332fe39337e7112c898c
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-05-21 22:42:59 +0600

    jwt token implimented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken')
 const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
 const res = require('express/lib/response');
@@ -12,12 +13,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'});
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+    })
+    next();
+}
+
 
 
 // const uri = `mongodb+srv://dbPlantPlanet:R6fioYRhJlhwjSWE@cluster0.rjowz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rjowz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -25,6 +40,17 @@ async function run() {
         await client.connect();
         const productCollection = client.db('plantPlanet').collection('products');
         const myProductCollection = client.db('plantPlanet').collection('myProducts')
+
+        //auth initialization
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+
+            res.send({ accessToken });
+        });
+
 
         //getting products 
         app.get('/products', async (req, res) => {
@@ -34,12 +60,7 @@ async function run() {
             res.send(products)
         });
 
-        app.get('/my-products', async (req, res) => {
-            const query = {};
-            const cursor = myProductCollection.find(query);
-            const products = await cursor.toArray();
-            res.send(products)
-        });
+        
 
         app.get('/update-product/:id', async (req, res) => {
             const id = req.params.id;
